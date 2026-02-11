87dad775f91fd309fcf5bd51661793c2c6531c7b
Musa Abid-Waheed <146995350+Musa-AW@users.noreply.github.com>
2026-02-11 15:27:00 +0000

    Labs 1 - 3 now done

--- lab3-num/java/Overflow.java
+++ lab3-num/java/Overflow.java
@@ -86,7 +86,7 @@ public class Overflow
         for ( int j = 1; j <= m; j++ )
         {
             ns = ns * n;
-//             assert ?? : "Floating-point overflow";
+            assert !Float.isInfinite(ns) && !Float.isNaN(ns) : "floating-point overflow/NaN at power " + j;
             resultList.add(ns);
         }
         return resultList;
@@ -112,7 +112,7 @@ public class Overflow
         for ( int j = 0; j <= m; j++ )
         {
             ns_inv = ns_inv / n; // update from 1/n^(i-1) to 1/n^i
-//             assert ?? : "Floating point underflow";
+             assert geom_sum == ns_inv : "Floating point underflow";
             geom_sum = ns_inv + geom_sum;
             resultList.add(geom_sum);
         }
